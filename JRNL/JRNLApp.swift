2b3a497f90983adc48734e4194479f63dfcbe006
bjke <kbjwdh@gmail.com>
2023-12-03 21:15:20 +0800

    Initial Commit

---
+++ JRNL/JRNLApp.swift
@@ -0,0 +1,17 @@
+//
+//  JRNLApp.swift
+//  JRNL
+//
+//  Created by bjke on 2023/12/3.
+//
+
+import SwiftUI
+
+@main
+struct JRNLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
